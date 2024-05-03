a1852a9b551b85267228e2ca8ca57aa65f255c7e
Christopher Chitimbwa <chitimbwasc@yahoo.com>
2024-05-03 14:20:21 +0200

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -17,8 +17,44 @@ display_image.display_image('https://upload.wikimedia.org/wikipedia/commons/4/45
 ################### FILE UPLOAD SECTION #######################
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'src'))
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import utils_v2
+from model_utils import *
+# Streamlit app
+def main(image_file):
+    model = utils_v2.retrieve_model()
+    
+    st.title("Mapping seagrass with Satellite Imagery and Deep Learning")
+    st.write("Choose an image to classify")
+    
+    # Choose an image file in the sidebar
+    # image_file = st.sidar.file_uploader("Choose an image file", type=["tif"])
+    
+    if image_file is not None:
+        # Display the chosen image
+        image = load_image(image_file)
+        X = preprocess_image(image)
+        y = model.predict(X)
+        y = np.squeeze(y, axis=0)
+        #st.image(image, caption="Chosen Image", use_column_width=True)
+        # Create a plot
+        plt.axis('off')
+        fig, (ax1, ax2) = plt.subplots(1, 2)
+        ax1.imshow(image[:, :, 3])
+        ax2.imshow(y)
+        # Display the image in streamlit
+        st.pyplot(fig)
+        # Make a prediction and display it
+        #prediction = predict(load_image(image_file))
+        #st.write("Prediction: ", prediction[1])
+        #st.write("Confidence: ", prediction[2])
 
 
-
+main(uploaded_file)
 
 font_modifier.make_font_poppins()
