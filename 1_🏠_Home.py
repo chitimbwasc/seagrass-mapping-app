da1bc31637491fa2b8a0a2dc7d785efc37b375c2
Christopher Chitimbwa <chitimbwasc@yahoo.com>
2024-05-04 10:16:26 +0300

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -25,9 +25,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 import utils_v2
 from model_utils import *
+
+
+
+
+@keras.saving.register_keras_serializable(package="my_package", name="dice_loss_plus_2focal_loss")
+def dice_loss_plus_2focal_loss(y_true, y_pred):
+    # Compute dice loss
+    dice_loss = sm.losses.DiceLoss(class_weights=weights)(y_true, y_pred)
+
+    # Compute focal loss
+    focal_loss = sm.losses.CategoricalFocalLoss()(y_true, y_pred)
+
+    # Combine dice loss and focal loss with a weighting factor
+    total_loss = dice_loss + (2 * focal_loss)
+
+    return total_loss
+
+@st.cache_resource
+def retrieve_model():
+    client = MlflowClient()
+    MLFLOW_TRACKING_URI = os.getenv('MLFLOW_TRACKING_URI')
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+    RUN_ID = os.getenv('best_model_run_id')
+    print(f'retrieving the model with run_id {RUN_ID}')
+    custom_objects = {"dice_loss_plus_2focal_loss": dice_loss_plus_2focal_loss}
+    with keras.saving.custom_object_scope(custom_objects):
+        model = mlflow.pyfunc.load_model("runs:/" + RUN_ID + "/model")
+    print(f'model with run_id {RUN_ID} retrieved')
+    return model
+
+
 # Streamlit app
 def main():
-    model = utils_v2.retrieve_model()
+    model = retrieve_model()
     
     st.title("Mapping seagrass with Satellite Imagery and Deep Learning")
     st.write("Choose an image to classify")
